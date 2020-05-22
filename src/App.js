73f68184d2affa4d21f4fa7552e30c193e1182b2
Jeovanni <jeovannildg@gmail.com>
2020-05-22 07:52:57 -0600

    postspage and searchbar imported to app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ Look at each file to see where you need to pass props or add code
 */
 import React from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage.js"
+import SearchBar from "./components/SearchBar/SearchBarContainer.js"
+
 // import the PostsPage and SearchBar and add them to the App
 
 
